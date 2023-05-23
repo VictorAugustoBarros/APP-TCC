c5b27ac6a078aaf407bfa2516bd5a718757cadd2
victor.barros <victor.barros@pgmais.com.br>
2023-05-23 05:06:51 -0300

    Alterando API_HOST

--- src/http/constants.js
+++ src/http/constants.js
@@ -1,4 +1,5 @@
-const API_HOST = "http://localhost:8000";
+// const API_HOST = "http://localhost:8000";
+const API_HOST = "http://ec2-18-230-69-64.sa-east-1.compute.amazonaws.com:6000";
 
 
 export default API_HOST;
