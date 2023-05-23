89b2acafdfdf53a237cea8d371833a9591df01a4
victor.barros <victor.barros@pgmais.com.br>
2023-05-23 05:16:29 -0300

    atualizando porta API

--- src/http/constants.js
+++ src/http/constants.js
@@ -1,5 +1,5 @@
 // const API_HOST = "http://localhost:8000";
-const API_HOST = "http://ec2-18-230-69-64.sa-east-1.compute.amazonaws.com:6000";
+const API_HOST = "http://ec2-18-230-69-64.sa-east-1.compute.amazonaws.com:8080";
 
 
 export default API_HOST;
