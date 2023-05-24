496a85bad717949ba26d563887168c881c010a4f
victor.barros <victor.barros@pgmais.com.br>
2023-05-24 13:21:55 -0300

    teste

--- src/http/constants.js
+++ src/http/constants.js
@@ -1,5 +1,5 @@
-// export const API_HOST = "http://localhost:8000"
-// export const ALLOW_ORIGIN = "http://localhost:3001"
+// export const API_HOST = "http://localhost:8081"
+// export const ALLOW_ORIGIN = "http://localhost:8082"
 
 export const API_HOST = "http://54.232.148.69:8081"
 export const ALLOW_ORIGIN = "http://54.232.148.69:8082"
