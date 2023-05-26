bcb0b7d681501e4b12343a4b0acc1fc5eda73787
victor.barros <victor.barros@pgmais.com.br>
2023-05-26 01:22:27 -0300

    teste

--- src/http/constants.js
+++ src/http/constants.js
@@ -1,5 +1,5 @@
 // export const API_HOST = "http://localhost:8081/api"
 // export const ALLOW_ORIGIN = "http://localhost:3001"
 
-export const API_HOST = "http://interlinker.com.br/api/"
-export const ALLOW_ORIGIN = "http://interlinker.com.br/"
+export const API_HOST = "http://interlinker.com.br/api"
+export const ALLOW_ORIGIN = "http://interlinker.com.br"
