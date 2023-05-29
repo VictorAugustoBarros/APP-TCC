34c100659faa4d83527471aec1b8b41f78dc9b46
victor.barros <victor.barros@pgmais.com.br>
2023-05-29 16:57:58 -0300

    teste

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  
   plugins: [
     vue({ 
       template: { transformAssetUrls }
@@ -18,7 +17,9 @@ export default defineConfig({
       autoImport: true,
     }),
   ],
-  
+  build: {
+    minify: false
+  },
   define: { 'process.env': {} },
   resolve: {
     alias: {
